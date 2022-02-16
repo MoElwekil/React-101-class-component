1228a954a4b22219bf5d083b13a482a5373e998d
Mo Elwekil <Mo@descod.net>
2022-02-16 00:01:23 +0000

    geolocation or erros can be now returned back to the user

--- src/index.js
+++ src/index.js
@@ -6,20 +6,25 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null }
-        console.log(this.state.lat);
+        this.state = { lat: null, errorMessage: '' }
 
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
-            err => console.log(err)
+            err => this.setState({ errorMessage: err.message })
         )
     }
 
 
     render() {
-        return (
-            <div>Latitude: {this.state.lat}</div>
-        )
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        return <div>Loading...</div>
     }
 }
 
